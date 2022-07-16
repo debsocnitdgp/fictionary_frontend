ab0012a61cf32c5f80de8e430dc70de7b8a3914c
Ayush Verma <itsmedoraemon19@gmail.com>
2022-07-17 00:34:11 +0530

    buttons done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Navbar from"./components/Navbar.js";
 // import Landing from './components/Landing.js';
-// import Question from './components/Question/Question.js';
-import Leaderboard from './components/Leaderboard/Leaderboard.js';
+import Question from './components/Question/Question.js';
+// import Leaderboard from './components/Leaderboard/Leaderboard.js';
 import './App.css';
 
 
@@ -11,8 +11,8 @@ const App = () => {
     <div className="bg">
       <Navbar />
       {/* <Landing /> */}
-      {/* <Question />  */}
-      <Leaderboard />
+      <Question /> 
+      {/* <Leaderboard /> */}
       
     </div>
   );
