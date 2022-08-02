eae1ff500863a25197f9ed1444d9af38f91ae5c3
Aiman <aimanaisha851@gmail.com>
2022-08-03 02:06:40 +0530

    responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,56 +1,67 @@
-
-import { Link } from 'react-router-dom';
-import "./Navbar.css"
-import Rules from './Rules/Rules';
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import Rules from "./Rules/Rules";
 import React, { useState } from "react";
 
-
 const Navbar = (props) => {
-      const [modalOpen, setmodalOpen] = useState(false);
+  const [modalOpen, setmodalOpen] = useState(false);
+
+  const showModalHandler = () => {
+    setmodalOpen(true);
+    console.log("true");
+  };
+  const hideModalHandler = () => {
+    setmodalOpen(false);
+    console.log("false");
+  };
 
   return (
-   <>
+    <>
       <nav className="main-nav">
-
-      <button onClick={props.showNav} className='open_btn'>
-          <svg xmlns="http://www.w3.org/2000/svg" className="open" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="1.5">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h8m-8 6h16" />
+        {modalOpen && <Rules onHideRules={hideModalHandler} />}
+        <button onClick={props.showNav} className="open_btn">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="open"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="1.5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M4 6h16M4 12h8m-8 6h16"
+            />
           </svg>
         </button>
 
-        <div className='middle'>
-        <div className="home">
-          <ul>
-            <Link to="/">Home</Link>
-          </ul>
-        </div>
- 
-        <div className="rules">
-          <ul>
-            <Link to="# " onClick={()=>{console.log('rule');setmodalOpen(true)}}>Rules</Link>
-          </ul>
-        </div>
+        <div className="middle">
+          <div className="home">
+            <ul>
+              <Link to="/">Home</Link>
+            </ul>
+          </div>
 
-        <div className="leader">
-          <ul>
-            <Link to="/leaderboard">Leaderboard</Link>
-          </ul>
-        </div> 
+          <div className="rules">
+            <ul>
+              <p className="rules" onClick={showModalHandler}>Rules</p>
+            </ul>
+          </div>
+
+          <div className="leader">
+            <ul>
+              <Link to="/leaderboard">Leaderboard</Link>
+            </ul>
+          </div>
         </div>
-        
 
         <div className="sign">
-        
-            <button className="si">SIGN IN</button>
-          
+          <button className="si">SIGN IN</button>
         </div>
-
       </nav>
-      <Rules open={modalOpen} handleclose={()=>setmodalOpen(false)} />
-      </>
-
-      
+    </>
   );
-}
+};
 
 export default Navbar;
--- src/components/NavbarResponsive.js
+++ src/components/NavbarResponsive.js
@@ -1,14 +1,23 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import './NavbarResponsive.css'
-import Rules from './Rules/Rules';
+import Rules from "./Rules/Rules";
 
 const NavbarResponsive = (props) => {
 
     const [modalOpen, setmodalOpen] = useState(false);
+    const showModalHandler = () => {
+        setmodalOpen(true)
+        console.log('true')
+    }
+    const hideModalHandler = () => {
+        setmodalOpen(false)
+        console.log('false')
+    }
 
     return(
         <div className={props.nav ? 'navbar_collapse_show' : 'navbar_collapse_hide'}>
+            {modalOpen && <Rules onHideRules={hideModalHandler}/>}
             <button className="close_btn" onClick={props.hideNav}>
                 <svg xmlns="http://www.w3.org/2000/svg" className="close" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
@@ -16,10 +25,9 @@ const NavbarResponsive = (props) => {
             </button>
             <div className="nav">
             <Link to="/" className='link'>HOME</Link>
-            <Link to="# " className='link' onClick={()=>{setmodalOpen(true)}}>RULES</Link>
+            <p className='rules' onClick={showModalHandler}>RULES</p>
             <Link to="/leaderboard" className='link'>LEADERBOARD</Link>
             </div>
-            <Rules open={modalOpen} handleclose={()=>setmodalOpen(false)} />
         </div>
     )
 }
