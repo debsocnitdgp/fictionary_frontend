9873952fc4032ae4665ed7a9e1fefab925ac19d8
Ayush Verma <itsmedoraemon19@gmail.com>
2022-08-09 11:54:24 +0530

    animations added

--- src/components/Leaderboard/Leaderboard.js
+++ src/components/Leaderboard/Leaderboard.js
@@ -1,15 +1,18 @@
 import React from "react";
 import "./Leaderboard.css";
 import Score from "./Score";
-
+import Stars from "./Stars"
 const Leaderboard = () => {
   return (
+    
     <div>
       <div className="img">
         <div>
           <h1>Leaderboard</h1>
         </div>
+        
         <Score />
+        <Stars />
       </div>
     </div>
   );
---
+++ src/components/Leaderboard/Stars.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import './Stars.scss';
+
+const Stars = () => {
+  return (
+    <div className="stars">
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+    </div>
+  );
+}
+
+export default Stars;
