27c628ad466fbbd272a2ae3e38becaa63245c407
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-11-06 13:50:07 +0530

    make timer on landing fetch from backend
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -30,7 +30,7 @@ const Landing = () => {
         <h1 className="fic">FICTIONARY </h1>
         <Stars />
       </div>
-      { false ? (
+      { gameLive.game_live ? (
         <div className="play_now">
           <Link to="/question" className="play">
             PLAY NOW
