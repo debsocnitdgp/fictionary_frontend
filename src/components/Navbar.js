4aee97838bb97953d63556352e8c62f4377447b4
Ayush Verma <itsmedoraemon19@gmail.com>
2023-01-03 17:36:49 +0530

    direct login from sign in

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,8 @@ import "./Navbar.css";
 import Rules from "./Rules/Rules";
 import React, { useState } from "react";
 import useContext from "../utils/Context";
+import { useGoogleLogin } from "@react-oauth/google";
+import endpoints from "../utils/APIendpoints";
 
 const Navbar = (props) => {
   const [modalOpen, setmodalOpen] = useState(false);
@@ -19,14 +21,40 @@ const Navbar = (props) => {
     console.log("false");
   };
 
-  const handleGoogleLogin = () => {
-    // const form = document.createElement("form");
-    // form.method = "POST";
-    // form.action = endpoints.GOOGLE_LOGIN;
-    // document.body.appendChild(form);
-    // form.submit();
-    navigate("/signin")
-  };
+  const handleGoogleLogin = useGoogleLogin({
+    onSuccess: (tokenResponse) => {
+      // setMessage("Fetching your information from Google...");
+      fetch(
+        "https://www.googleapis.com/oauth2/v3/userinfo?access_token=" +
+          tokenResponse.access_token
+      )
+        .then((json) => json.json())
+        .then((res) => {
+          // setMessage("Signing you in...");
+          fetch(endpoints.SOCIAL_LOGIN_TOKEN, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              access_token: tokenResponse.access_token,
+              ...res,
+            }),
+          })
+            .then((res) => res.json())
+            .then((res) => {
+              // setMessage("Logged in succesfully. Redirecting...");
+              context.login(res.token);
+              navigate("/question");
+            })
+            .catch((err) => console.error(err));
+        });
+    },
+    onFailure: (error) => {
+      console.log(error);
+    },
+  });
+
   const handleLogOut = () => {
     context.logout();
     navigate("/");
