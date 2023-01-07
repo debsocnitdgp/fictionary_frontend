df882c701ba10586829f898013b982d18196681b
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2023-01-07 20:06:06 +0530

    Update GameOver.js

--- src/pages/GameOver/GameOver.js
+++ src/pages/GameOver/GameOver.js
@@ -11,7 +11,7 @@ const Login = () => {
     <div className={styles.container}>
       <div className={styles.box}>
         <h1>Congratulations!</h1>
-        <div className={styles.message}>You have finished the game!</div>
+        <div className={styles.message}>You have finished today's rounds! Come back tomorrow for more!</div>
         <div className={styles.message}>Click below to view the leaderboard</div>
         <button className={styles.btn} onClick={() => navigate("/leaderboard")}>Go to Leaderboard</button>
       </div>
